ca69b74c46048c0f76eb5e7f95de1927c88174be
ANDREWAHN-UK <77354731+ANDREWAHN-UK@users.noreply.github.com>
2025-02-10 15:48:50 +0000

    add majority of the js code, to create buttons, stop a song if one is playing, play the clicked on song. song means sound here

--- Project 9/project9.js
+++ Project 9/project9.js
@@ -1,9 +1,23 @@
-const sounds = ['applause', 'boo', 'failure', 'tada', 'victory', 'gasp' ]
+const sounds = ["applause", "boo", "failure", "tada", "victory", "gasp"];
 
-sounds.forEach(sound => {
-const btn = document.createElement('button')
-btn.classList.add('btn')
-btn.innerText = sound
-document.getElementById('buttons').
-appendChild(btn)
-})
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.innerText = sound;
+
+  btn.addEventListener("click", () => {
+    stopSongs()
+    document.getElementById(sound).play();
+  });
+
+  document.getElementById("buttons").appendChild(btn);
+});
+
+
+function stopSongs(){
+  sounds.forEach(sound => {
+    const song = document.getElementById(sound)
+    song.pause()
+    song.currentTime = 0;
+  })
+}
