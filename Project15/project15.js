f14d4d71342939a73c181b5f62337cfc0ecc0f42
ANDREWAHN-UK <77354731+ANDREWAHN-UK@users.noreply.github.com>
2025-03-03 11:10:56 +0000

    adjust project 15, touch up base css

--- Project15/project15.js
+++ Project15/project15.js
@@ -1,73 +1,21 @@
-const tagsEl = document.getElementById("tags");
-const textarea = document.getElementById("textarea");
+const counters = document.querySelectorAll(".counter");
 
-textarea.focus();
+counters.forEach((counter) => {
+  counter.innerText = "0";
 
-// below says that id the enter key is detected, then to create the tags and start the random selector
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const c = +counter.innerText;
 
-textarea.addEventListener("keyup", (e) => {
-  createTags(e.target.value);
-  if (e.key === "Enter") {
-    setTimeout(() => {
-      e.target.value = "";
-    }, 10);
-    randomSelect();
-  }
+    const increment = target / 1000;
+
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`
+      setTimeout(updateCounter, 1)
+    } else {
+      counter.innerText = target
+    }
+  };
+
+  updateCounter();
 });
-
-// This function creates a tag based on user input
-
-function createTags(input) {
-  const tags = input
-    .split(",")
-    .filter((tag) => tag.trim() !== "")
-    .map((tag) => tag.trim());
-
-  tagsEl.innerHTML = "";
-
-  tags.forEach((tag) => {
-    const tagEl = document.createElement("span");
-    tagEl.classList.add("tag");
-    tagEl.innerText = tag;
-    tagsEl.appendChild(tagEl);
-  });
-}
-
-// This function randomly selects a tag, highlights them randomly and then stops on one tag
-
-function randomSelect() {
-  const times = 30;
-
-  const interval = setInterval(() => {
-    const randomTag = pickRandomTag();
-
-    highlightTag(randomTag);
-    setTimeout(() => {
-      unhighlightTag(randomTag);
-    }, 100);
-  }, 100);
-
-  setTimeout(
-    ()=> {
-        clearInterval(interval)
-
-        setTimeout(() =>{
-            const randomTag = pickRandomTag()
-            highlightTag(randomTag)
-        })
-    }, times * 100
-  )
-}
-
-function pickRandomTag() {
-  const tags = document.querySelectorAll(".tag");
-  return tags[Math.floor(Math.random() * tags.length)];
-}
-
-function highlightTag(tag) {
-  tag.classList.add("highlight");
-}
-
-function unhighlightTag(tag) {
-  tag.classList.remove("highlight");
-}
