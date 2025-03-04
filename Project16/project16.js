0b4c23a58b65e52d3ecf5354da99e31ba8822013
ANDREWAHN-UK <77354731+ANDREWAHN-UK@users.noreply.github.com>
2025-03-04 12:20:51 +0000

    finish project 16

--- Project16/project16.js
+++ Project16/project16.js
@@ -1,21 +1,59 @@
-const counters = document.querySelectorAll(".counter");
+const smallCups = document.querySelectorAll(".cup-small");
 
-counters.forEach((counter) => {
-  counter.innerText = "0";
+const litres = document.getElementById("litres");
 
-  const updateCounter = () => {
-    const target = +counter.getAttribute("data-target");
-    const c = +counter.innerText;
+const percentage = document.getElementById("percentage");
 
-    const increment = target / 1000;
+const remained = document.getElementById("remained");
 
-    if (c < target) {
-      counter.innerText = `${Math.ceil(c + increment)}`
-      setTimeout(updateCounter, 1)
-    } else {
-      counter.innerText = target
-    }
-  };
+updateBigCups
 
-  updateCounter();
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener("click", () => highlightCups(idx));
 });
+
+
+// this function updates the small cups, as well as the single large cup
+function highlightCups(idx) {
+// this bit checks if the cup you are clicking is full, and the one next to it is not full, and decrements by one
+  if(smallCups[idx].classList.contains('full') && !smallCups[idx].nextElementSibling.classList.contains('full')){
+    idx --
+  }
+
+  // this bit adds and removes the class full as needed
+  smallCups.forEach((cup, idx2) => {
+    if (idx2 <= idx) {
+      cup.classList.add("full");
+    } else {
+      cup.classList.remove("full");
+    }
+  });
+
+  updateBigCups()
+}
+
+function updateBigCups(){
+  const fullCups = document.querySelectorAll('.cup-small.full').length
+
+  const totalCups = smallCups.length
+
+  if (fullCups === 0){
+    percentage.style.visibility = 'hidden'
+     percentage.style.height = 0
+  } else {
+    percentage.style.visibility = 'visible'
+    percentage.style.height = `${fullCups/totalCups * 330}px`
+    percentage.innerText = `${fullCups/totalCups * 100}% `
+  }
+
+  if(fullCups === totalCups){
+    remained.style.height=0
+    remained.style.visibility='hidden'
+   
+   
+  } else {
+    remained.style.visibility='visible'
+    litres.innerText = `${2 -(250 * fullCups / 1000)}L remaining`
+  }
+
+}
